a1e6a6efb7fbb586aa8c0c925b68d335ae309263
Zouthen <mich5n69@stud.kea.dk>
2023-02-22 09:34:04 +0100

    student med h2

--- src/main/java/com/example/studentv1/controller/StudentRestController.java
+++ src/main/java/com/example/studentv1/controller/StudentRestController.java
@@ -11,7 +11,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @RestController
-public class StudentRestContoller {
+public class StudentRestController {
 
     @Autowired
     StudentRepository studentRepository;
